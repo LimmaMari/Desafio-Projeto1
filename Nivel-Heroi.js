0451314a97e94e4599b3399e7a4499cfe03d85be
LimmaMari <marinalima.biju@gmail.com>
2024-05-04 11:52:30 -0300

    nivel do heroi projeto1

---
+++ Nivel-Heroi.js
@@ -0,0 +1,15 @@
+//entrada
+let nickname = "Marina "
+let mensagemLuta = "Foi convidado para Lutar"
+let mensagemMissao = "Recebeu uma nova Missão "
+//variavel number
+let Ferro = 1000>0 && "Ferro. "
+let Bronze = 2000>1001 && "Bronze. " 
+let Prata = 5000>2001 && "Prata. "
+let Ouro = 7000>5001 && "Ouro. "
+let Platina = 8000>7001 && "Platina. "
+let Ascendente = 9000>8001 && "Ascendente. "
+let Imortal = 10000>9001 && "Imortal. "
+let Radiante = <10001 && "Radiante. "
+//saida
+console.log("O Herói de Nome " + nickname + "Esta no nível " + Bronze + nickname + mensagemLuta + " e " + mensagemMissao)
